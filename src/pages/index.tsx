87b128907b3c02f9ade919c6e5160bc179e8dccf
Mauricio Girardi <maurigirarde@yahoo.com.br>
2021-03-10 19:28:13 -0300

    add styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import Main from '../components/Main'
+import GlobalStyles from '../styles/GlobalStyles'
 
 export default function Home() {
   return (
     <div>
+      <GlobalStyles />
       <Main />
     </div>
   )
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  button,
+  body,
+  textarea,
+  input {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  body,
+  html {
+    height: 100%;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  @media (max-width: 1000px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+`
