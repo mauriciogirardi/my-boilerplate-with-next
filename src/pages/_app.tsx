8c4c6e23a7e5a8dcb2747bcc9c83b6d5ea738249
Mauricio Girardi <maurigirarde@yahoo.com.br>
2021-03-11 11:54:04 -0300

    fix config PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,14 +8,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>My Boilerplate</title>
-        <link rel="shortcut icon" href="img/icon-192.png" />
-        <link rel="apple-touch-icon" href="img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A simple project starter to work with Typescript, React, NextJs and Styled Components"
-        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,7 +39,14 @@ export default class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <meta charSet="utf-8" />
-
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="img/icon-192.png" />
+          <link rel="apple-touch-icon" href="img/icon-512.png" />
+          <meta name="theme-color" content="#06092B" />
+          <meta
+            name="description"
+            content="A simple project starter to work with Typescript, React, NextJs and Styled Components"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
